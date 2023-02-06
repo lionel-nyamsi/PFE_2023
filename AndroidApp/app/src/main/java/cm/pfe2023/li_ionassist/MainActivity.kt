fdd65d7fbd80ca94911af2970210b61822ca369f
[DuvalMambou] <[duvalmambou@gmail.com]>
2023-02-06 08:35:39 +0100

    Finalisation du developpement front-end de l'application sur Android

--- AndroidApp/app/src/main/java/cm/pfe2023/li_ionassist/MainActivity.kt
+++ AndroidApp/app/src/main/java/cm/pfe2023/li_ionassist/MainActivity.kt
@@ -2,17 +2,20 @@ package cm.pfe2023.li_ionassist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import cm.pfe2023.li_ionassist.fragments.ConnectionFragment
 import cm.pfe2023.li_ionassist.fragments.RegistrationFragment
+import cm.pfe2023.li_ionassist.fragments.ScanCodeFragment
+import cm.pfe2023.li_ionassist.fragments.WelcomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.registrer_page)
+        setContentView(R.layout.activity_main)
 
-        // Injecter le bloc de register
+        /* Injecter le fragment de register
         val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.frament_container, RegistrationFragment())
+        transaction.replace(R.id.fragment_container, RegistrationFragment())
         transaction.addToBackStack(null)
-        transaction.commit()
+        transaction.commit()*/
     }
 }
